fc5f2cefae45dd5ba639d2d2d170cdbb6c59f8a6
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-03 14:14:58 +0530

    add variables

--- main.go
+++ main.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math/rand" //small letters and as short as possible
 	"math"
+	"math/cmplx"
+	"math/rand" //small letters and as short as possible
+)
+
+var c, python, java bool //variable is not in unintialize, always in zero values(lower bound) OUTSIDE OF FUNCTION
+
+var (
+	Tobe bool = false
+	MaxInt uint64 = 1<<64 -1
+	z complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
+
+	var i int
+	fmt.Println(i, c, java, python)
+	j := 9
+	var z = 10
+	var s string = "heo"
+	fmt.Println(j, z, s)
+
+	fmt.Printf("Type: %T Value: %v\n", Tobe, Tobe);
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	var c int
+	var f float64
+	var d bool
+	var st string
+	fmt.Printf("%v %v %v %q\n", c, f, d, st)
+
 	fmt.Println("Hello, 世界")
 	genRandNum()
 	fmt.Println(math.Pi) //encapsulation: always access with capital letters if in another package
